56ff4cdecd8eec36d351eed92a999330165acad2
Yuya Maeda <6yu.disney22xx@gmail.com>
2021-09-29 19:25:25 +0900

    test commit.

--- src/TryFinally.java
+++ src/TryFinally.java
@@ -1,7 +1,16 @@
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 
+/**
+ * @author yyM06
+ *
+ */
 class TryFinally {
 
-  protected void copy(String src, String dest) throws IOException {
+  protected void tryFinallyCopy(String src, String dest) throws IOException {
     InputStream in = new FileInputStream(src);
     try {
         OutputStream out = new FileOutputStream(dest);
@@ -15,12 +24,24 @@ class TryFinally {
             out.close();
         }
     } finally {
-        if (br != null)
-        try {
-            br.close();
-        } catch (IOException e) {
-            // 例外処理
+        if (in != null)
+	        try {
+	            in.close();
+	        } catch (IOException e) {
+	            // 例外処理
+	        }
+	}
+  }
+  
+  
+  protected void tryWithResourceCopy(String src, String dest) throws IOException {
+    try (InputStream inputStream = new FileInputStream(src); OutputStream outputStream = new FileOutputStream(dest)) {
+        byte[] buf = new byte[100];
+        int n;
+        while ((n = inputStream.read(buf)) >= 0) {
+            outputStream.write(buf, 0, n);
         }
-		}
-}
+    }
+  }
+  
 }
