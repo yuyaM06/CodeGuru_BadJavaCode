158d35d1529ae85edf693203d3b50a9c403af080
Yuya Maeda <6yu.disney22xx@gmail.com>
2021-10-03 10:04:49 +0900

    test commit.

--- src/TryFinally.java
+++ src/TryFinally.java
@@ -43,24 +43,5 @@ class TryFinally {
           }
       }
   }
-  
-  
-  protected void tryWithResourceCopy(String src, String dest) throws IOException {
-	  String inFilePath = "D:\\foo.txt";
-      String outFilePath = "D:\\bar.txt";
 
-      // tryブロックを抜けるときにリソースを解放
-      try (FileInputStream in = new FileInputStream(inFilePath);
-              FileOutputStream out = new FileOutputStream(outFilePath);) {
-          int c;
-
-          // データコピー
-          while ((c = in.read()) != -1) {
-              out.write(c);
-          }
-      } catch (IOException e) {
-          e.printStackTrace();
-      }
-  }
-  
 }
